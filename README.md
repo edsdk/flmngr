d1f2d154d46d4e2c24250dcdec231aad1fc3e987
dmitriy-komarov <dmitriy-komarov@3627127.no-reply.drupal.org>
2022-11-13 21:02:39 +0400

    A new version, fully reimagined and redeveloped.

--- README.md
+++ README.md
@@ -1,123 +1,68 @@
-# Flmngr file manager SDK
+<p align="center">
+    <a href="https://flmngr.com/"><img src="https://flmngr.com/img/Flmngr.png" alt="Flmngr" width="90" /></a>
+</p>
 
-> File manager SDK for your websites and applicatoins. Both **client** and **server** side modules.
+<h1 align="center" style="margin-top:-20px">Flmngr</h1>
 
-**Flmngr** is full featured file manager for your current or future app or website. You can add this module to instantly have feature to manage files on your webserver: to allow users to upload, edit and choose files, to build a structure for your files and images, to delete or download them.
+<p align="center">
+    <strong>Flmngr file manager SDK for JS and TS. PHP backend is included.</strong>
+</p>
 
-It can be perfectly integrated with common CMSs (WordPress, Drupal, Joomla, etc), with popular client frameworks (React, Angular, Vue, etc.), server frameworks (Laravel, Symphony, YII, RoR, Django, etc.) and in any other code using API.
+<p align="center">
+    <a href="https://flmngr.com/">Home</a>&nbsp;&nbsp;∙&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://flmngr.com/doc/install-npm-package/">Install</a>&nbsp;&nbsp;∙&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://flmngr.com/doc/api">API with samples</a>
+</p>
 
-The great advantage of Flmngr are tools for full stack application integration.
-Flmngr contains both client script (JS/TypeScript) and [server side](https://npmjs.com/package/@edsdk/flmngr-server) in **PHP**, **Node** and **Java** for saving images on your server. It also has microservice feature for those who would like to use uploader separately or uses different language on server side.
+[![Flmngr file manager screenshot](https://flmngr.com/img/browsing.jpg)](https://flmngr.com)
 
-Deploy and run your **own demo in 1 min** using [Flmngr example](https://github.com/edsdk/flmngr-example) repository.
+Flmngr is a **JavaScript** / **TypeScript** library which lets you upload, manage files, edit images and re-upload them onto your server, and create different image formats (resized variants).
 
+It can be used as a **file manager** when you wish to let the user have a feature just to manage files on the server.
 
+Also, you can use it in **file picker** mode when you need to let the user select some file or image on your server. This is useful for handling **file inputs** and **managing image galleries** (you can pass to the file manager preselected files and retrieve a new set after a user closes the dialog).
+
+Built-in **image editor** gives you a feature to edit images right in the browser.
+
+**Powerful API** is a base for creating very custom scenarios.
+
+Your files are your files, Flmngr does not lock you in a cloud: **PHP backend** for your server is available. But when you need **Amazon S3** and **Azure Blob** adapters can be installed.
+
+You can use Flmngr in any application from custom ones (by using this **NPM package**) to **React**, **Vue**, or **any framework** apps. 
+
+Flmngr is so flexible that has integration for popular CMSs like **Drupal** and WYSIWYG editors such as **TinyMCE**, **CKEditor 4**, and **CKEditor 5**.
 
 ## Install
 
-With [npm](https://npmjs.com/) installed, run
+Using NPM:
 
 ```
-$ npm install @edsdk/flmngr
+npm i --save flmngr
 ```
 
-Yarn users can run
+Using Yarn:
 
 ```
-$ yarn add @edsdk/flmngr
+yarn add flmngr
 ```
 
+## Sample
 
-## Usage
+The sample of just one feature - to open a file manager to select a single file. See [more samples](https://flmngr.com/doc/api).
 
 ```js
-    openFlmngr({
-		urlFileManager: 'http://localhost:8080/flmngr/',
-		onOk: (files: IFile[]) => {
-            for (const file of files)
-                console.log(`${file.name} (${file.size})`);
-        },
-    });
-```
+import {Flmngr} from "flmngr";
 
-This code immediately opens Flmngr fullscreen dialog in your browser letting user to specify some files, probably with uploading them. When user confirms its selection, the dialog is being closed and `onOk` callback is processed. In this password we just print info about files into console.
-
-You need to have [@edsdk/flmngr-server package](https://npmjs.com/package/@edsdk/flmngr-server) installed and started on the URL equal to `urlFileManager` you've passed to your frontend part of Flmngr.
-
-
-## API
-
-```js
-function openFlmngr({
-    urlFileManager: string,
-    onOk: (
-        files: IFile[]
-    ) => void
-
-    onCancel?: () => void,
-    onSelected?: (files: IFile[]) => null | boolean
-    onlyImages?: boolean,
-    isMultiple?: boolean,
-    urlFiles?: string,
-    dateFormat?: string,
-    openLastDir?: boolean,
-    branding?: boolean,
-    maxFolderTreeWidth?: boolean,
-    isIconsView?: true,
+Flmngr.open({
+    apiKey: "FLMNFLMN",                                  // default free key
+    urlFileManager: 'https://fm.flmngr.com/fileManager', // demo server
+    urlFiles: 'https://fm.flmngr.com/files',             // demo file storage
+    
+    onFinish: (files) => {
+        console.log("User picked:")
+        console.log(files);
+    }
 });
 ```
 
-- `urlFileManager` - URL of Flmngr server in binded to (be sure CORS is enabled for external resources)
-- `onOk` - callback for files specified and "Ok" button is pressed event
-
-Optional parameters:
-
-- `onCancel` - callback for case user had closed Flmngr without selecting a file
-- `onSelected` - callback called on any files selection change. It gets files array as argument and returns the flag of ability to choose exactly this files. Return `true` if you want to set "Ok" button enabled, `false` to disable it or `null` if you want to make it enabled or disabled based on flags `onlyImages` and `isMultiple` (default)
-- `onlyImages` - show and allow to choose images only, default is `false`
-- `isIconsView` - show files as icons or as table. `true` is default
-- `isMultiple` - allow to choose many files or just once, default is `true`
-- `urlFiles` - URL prefix to uploaded files i. e. `https://somesite.com/files/`
-- `dateFormat` - format to print all file time attributes in, `DD/MM/YYYY HH:mm` by default
-- `openLastDir` - do Flmngr to open last opened directoty on future Flmngr uses. `fm_lastDir` cookie is used for saving it. This option affects only if you open Flmngr without files preselected. Default is `true`
-- `branding` - do show name of Flmngr in UI, default is `true`
-- `maxFolderTreeWidth` - maximum width of directories view, `250` is default
-- `isIconsView` - do show previews of images or table view is default, default is `true`
-
-
-#### Preloading
-
-To avoid network delays you can preload Flmngr at any moment (e. g. you page is loaded):
-
-```js
-function preload(callback?: () => void);
-```
-
-After this call all next `openFlmngr` calls will be faster. If you do not use `preload`, calling `openFlmngr` first time can be slower.
-You can also pass `callback` function if you want to execute some code right after Flmngr libraries were preloaded.
-
-
-## See Also
-
-- Website: [flmngr.com](https://flmngr.com)
-- ImgPen image editor: [imgpen.com](https://imgpen.com)
-- Flmngr backend package: [npm package](https://npmjs.org/package/@edsdk/flmngr-server)  |  [GitHub project](https://github.com/edsdk/flmngr-server)
-- Flmngr example: [npm package](https://npmjs.org/package/@edsdk/flmngr-example)  |  [GitHub project](https://github.com/edsdk/flmngr-example)
-
-
 ## License
 
-Double licensing:
-
-1. Trial EdSDK license
-    - All features
-    - NOT for commercial usage (except trial purposes on dev server)
-    - [Server side](https://npmjs.com/package/@edsdk/flmngr-server) in TypeScript/JavaScript only.
-
-2. Commercial EdSDK license
-    - All features
-    - Commercial usage is allowed
-    - No "powered by" link is required
-    - Node (JavaScript, TypeScript), Java and PHP backends
-    - OEM usage in applications is an option
-    - [Purchase a license](https://flmngr.com) in order to use it
+This NPM package is licensed under LGPL 2.0 or later.
---
+++ src/index.ts
@@ -0,0 +1,436 @@
+interface CSRFTokenResult {
+    headers?: {[key: string]: string};
+    params?: {[key: string]: string};
+}
+
+type FuncGetCSRFToken = (
+    onSuccess: (
+        result: CSRFTokenResult
+    ) => void,
+    onError: () => void
+) => void;
+
+interface FlmngrImageFormat {
+    id: string;
+    title: string;
+    suffix: string;
+    maxWidth: number|null;
+    maxHeight: number|null;
+}
+
+export interface FlmngrFile {
+    format: string; // FlmngrImageFormat.id
+    url: string;
+    isServerFile: boolean;
+}
+
+export interface FlmngrFileWithFormats extends FlmngrFile {
+    formats: FlmngrFile[];
+}
+
+interface FlmngrLoadParams {
+    waitForImgPenToo?: boolean; // default true
+}
+
+export enum FlmngrUploadMode {
+    AUTORENAME ="AUTORENAME",
+    OVERWRITE = "OVERWRITE"
+}
+
+interface FlmngrCreateParams extends FlmngrLoadParams {
+
+    apiKey?: string; // you may omit it if you have already called Flmngr.load(apiKey) method
+
+    urlFiles: string;
+
+    urlFileManager: string;
+    urlFileManager__CSRF?: FuncGetCSRFToken|null;
+    urlFileManager__user?: string;
+    urlFileManager__password?: string;
+
+    accessKey?: string; // a secret key to access through the cloud backend at files.flmngr.com
+
+    apiKeyUnsplash?: string;
+
+    isMaximized?: boolean; // default false
+    showMaximizeButton?: boolean; // default true
+    showCloseButton?: boolean; // default true
+
+    hideFiles?: string[];
+    hideDirs?: string[];
+
+    defaultUploadDir?: string;
+    uploadThreads?: number;
+
+    filesPerPortion?: number; // default 100
+
+    imageFormats?: FlmngrImageFormat[];
+}
+
+interface FlmngrOpenParams extends FlmngrCreateParams {
+    list?: null|string[];
+
+    acceptExtensions?: string[];
+
+    isMultiple?: boolean;
+    allowReorder?: boolean;
+
+    retrieveImageFormats?: string[];
+
+    onFinish?: (files: FlmngrFileWithFormats[]) => void;
+    onCancel?: () => void;
+}
+
+export interface FlmngrServerFile {
+    url: string;
+
+    name: string; // file name with ext
+    size: number; // in bytes
+    timestamp: number; // in msec
+
+    // For images only
+    blurHash?: string;
+    width?: number;
+    height?: number;
+    formats?: {[key: string]: FlmngrServerFile}; // another image formats
+}
+
+
+interface FlmngrUploadParams extends FlmngrCreateParams {
+    filesOrLinks: (File|string)[];
+    dirUploads?: string;
+    mode?: FlmngrUploadMode;
+
+    onFinish?: (files: FlmngrServerFile[]) => void;
+    onFail?: (error: string) => void;
+}
+
+interface FlmngrEditAndUploadParams extends FlmngrCreateParams {
+    url: string; // URL or base64
+    filename?: string; // filename to display (i. e. when passing base64 as url)
+    dirUploads?: string; // used by default for external files
+    onSave?: (urlNew: string) => void;
+    onCancel?: () => void;
+}
+
+enum FlmngrImageFormatCreationMode {
+    ALWAYS = "ALWAYS",
+    IF_EXISTS = "IF_EXISTS",
+    DO_NOT_UPDATE = "DO_NOT_UPDATE"
+}
+
+interface FlmngrCreateImageFormatsParams extends FlmngrCreateParams {
+    urls: string[];
+    createImageFormats: {[key: string]: FlmngrImageFormatCreationMode};
+
+    showProgress?: boolean;
+
+    onFinish: (result: {[url: string]: {[formatId: string]: string}}) => void;
+    onProgress?: (finished: number, failed: number, total: number) => void;
+    onCancel?: () => void;
+}
+
+interface FlmngrEditParams extends FlmngrCreateParams {
+    url: string; // URL or base64
+    onSave: (
+        onExport: (
+            name: string,
+            ext: string,
+            jpegQuality: number,
+            onExported: (image: File) => void,
+            onFail: () => void
+        ) => void,
+        onClose: () => void
+    ) => void;
+    onCancel?: () => void;
+}
+
+interface FlmngrSelectUrlsParams {
+    apiKey?: string,
+    isMultiple?: boolean,
+    onFinish: (urls: string[]) => void,
+    onCancel?: () => void
+}
+
+export class Flmngr {
+
+    private static commonParams: Partial<FlmngrCreateParams> = null;
+    public static load(
+        params: Partial<FlmngrCreateParams>,
+        on?: {
+            onFlmngrAndImgPenLoaded?: () => void;
+            onFlmngrLoaded?: () => void;
+            onImgPenLoaded?: () => void;
+        }
+    ) {
+        if (!!Flmngr.commonParams && !!Flmngr.commonParams.apiKey && !!params.apiKey && Flmngr.commonParams.apiKey !== params.apiKey)
+            throw "Flmngr was called with another API key before:" + Flmngr.commonParams.apiKey + ". Now you call it with API key: " + params.apiKey + ".\nYou can not mix different API keys on one page";
+
+        if (!Flmngr.commonParams.apiKey && !params.apiKey)
+            throw "You must pass API key on the first Flmngr usage";
+
+        let callbacks: {
+            onFlmngrAndImgPenLoaded?: () => void;
+            onFlmngrLoaded?: () => void;
+            onImgPenLoaded?: () => void;
+        } = {};
+        if (!!on) {
+            callbacks = {...on};
+        }
+
+        if (!!callbacks.onFlmngrAndImgPenLoaded) {
+            if (this.isFlmngrLoaded() && this.isImgPenLoaded()) {
+                callbacks.onFlmngrAndImgPenLoaded();
+                delete callbacks.onFlmngrAndImgPenLoaded;
+            }
+        }
+
+        if (!!callbacks.onFlmngrLoaded) {
+            if (this.isFlmngrLoaded()) {
+                callbacks.onFlmngrLoaded();
+                delete callbacks.onFlmngrLoaded;
+            }
+        }
+
+        if (!!callbacks.onImgPenLoaded) {
+            if (this.isImgPenLoaded()) {
+                callbacks.onImgPenLoaded();
+                delete callbacks.onImgPenLoaded;
+            }
+        }
+
+        // Do not override base parameters on second time calls like Flmngr.open() where parameters may be overridden.
+        // API key at this place is guaranteed the same, so there is no need to set it again.
+        if (!!Flmngr.commonParams)
+            Flmngr.commonParams = params;
+
+        if (!!on && !!on.onFlmngrAndImgPenLoaded) {
+            if (!(window as any).onFlmngrAndImgPenLoadedArray)
+                (window as any).onFlmngrAndImgPenLoadedArray = [];
+            (window as any).onFlmngrAndImgPenLoadedArray.push(() => {
+                on.onFlmngrAndImgPenLoaded();
+            });
+        }
+
+        if (!!on && !!on.onFlmngrLoaded) {
+            if (!(window as any).onFlmngrLoadedArray)
+                (window as any).onFlmngrLoadedArray = [];
+            (window as any).onFlmngrLoadedArray.push(() => {
+                on.onFlmngrLoaded();
+            });
+        }
+
+        if (!!on && !!on.onImgPenLoaded) {
+            if (!(window as any).onImgPenLoadedArray)
+                (window as any).onImgPenLoadedArray = [];
+            (window as any).onImgPenLoadedArray.push(() => {
+                on.onImgPenLoaded();
+            });
+        }
+
+        Flmngr.includeJS(((params as any).cdnHost || "https://cloud.flmngr.com" ) + "/cdn/" + Flmngr.commonParams.apiKey + "/flmngr.js");
+        Flmngr.includeJS(((params as any).cdnHost || "https://cloud.flmngr.com" ) + "/cdn/" + Flmngr.commonParams.apiKey + "/imgpen.js");
+    }
+
+    private static includeJS(urlJS: string) {
+        let scripts = document.getElementsByTagName("script");
+        let alreadyExists = false;
+        let existingScript = null;
+        for (let i = 0; i < scripts.length; i++) {
+            let src = decodeURI(scripts[i].getAttribute("src"));
+            if (src != null && src.indexOf(urlJS) !== -1) {
+                alreadyExists = true;
+                existingScript = scripts[i];
+            }
+        }
+        if (!alreadyExists) {
+            let script = document.createElement("script");
+            script.type = "text/javascript";
+            script.src = urlJS;
+            script.setAttribute("data-by-n1ed", "true");
+            document.getElementsByTagName("head")[0].appendChild(script);
+            return script;
+        } else {
+            return null;
+        }
+    }
+
+    public static isLoaded(checkImgPenToo: boolean): boolean {
+        return (
+            Flmngr.isFlmngrLoaded() &&
+            (!checkImgPenToo || Flmngr.isImgPenLoaded())
+        );
+    }
+
+    public static isFlmngrLoaded(): boolean {
+        return !!(window as any).flmngr;
+    }
+
+    public static isImgPenLoaded(): boolean {
+        return !!(window as any).imgpen;
+    }
+
+    private static waitForLoaded(params: Partial<FlmngrCreateParams>, onLoaded: () => void) {
+        if (Flmngr.isLoaded(params.waitForImgPenToo)) {
+            onLoaded();
+        } else {
+
+            let loadParams: Partial<FlmngrCreateParams> = {};
+            if (!!Flmngr.commonParams) {
+                loadParams = {
+                    ...Flmngr.commonParams
+                };
+            }
+            if (!!params) {
+                loadParams = {
+                    ...loadParams,
+                    ...params
+                }
+            }
+            if (!!params.apiKey)
+                loadParams.apiKey = params.apiKey;
+            delete loadParams.waitForImgPenToo; // may be passed but won't be saved
+
+            Flmngr.load(
+                loadParams,
+                params.waitForImgPenToo ? {onFlmngrAndImgPenLoaded: onLoaded} : {onFlmngrLoaded: onLoaded}
+            );
+        }
+    }
+
+    public static open(params: FlmngrOpenParams) {
+        Flmngr.waitForLoaded(params, () => {
+            let mergedParams = {
+                ...this.commonParams,
+                ...params
+            };
+            if (params.isMultiple !== null && !mergedParams.onFinish)
+                throw "`onFinish` listener is required for `Flmngr.open()` method and wait for some file to be picked (when `isMultiple` != null)";
+            (window as any).flmngr.open(mergedParams);
+        });
+    }
+
+    public static upload(params: FlmngrUploadParams) {
+        Flmngr.waitForLoaded(params, () => {
+            let mergedParams = {
+                ...this.commonParams,
+                ...params
+            };
+            if (!mergedParams.filesOrLinks)
+                throw "`filesOrLinks` parameter is required to call `Flmngr.upload()` method";
+            (window as any).flmngr.upload(mergedParams);
+        });
+    }
+
+    public static createImageFormats(params: FlmngrCreateImageFormatsParams) {
+        Flmngr.waitForLoaded(params, () => {
+            let mergedParams = {
+                ...this.commonParams,
+                ...params
+            };
+            (window as any).flmngr.createImageFormats(mergedParams);
+        });
+    }
+
+    public static edit(params: FlmngrEditParams) {
+        let forcedParams = {
+            ...params,
+            waitForImgPenToo: true
+        };
+        Flmngr.waitForLoaded(forcedParams, () => {
+            let mergedParams = {
+                ...this.commonParams,
+                ...params
+            };
+            (window as any).flmngr.edit(mergedParams);
+        });
+    }
+
+    public static editAndUpload(params: FlmngrEditAndUploadParams) {
+        let forcedParams = {
+            ...params,
+            waitForImgPenToo: true
+        };
+        Flmngr.waitForLoaded(forcedParams, () => {
+            let mergedParams = {
+                ...this.commonParams,
+                ...params
+            };
+            (window as any).flmngr.editAndUpload(mergedParams);
+        });
+    }
+
+    public static getImageExtensions(): string[] {
+        return ['png', 'jpeg', 'jpg', 'webp', 'svg', 'gif', 'bmp'];
+    }
+
+    public static getNoCacheUrl(url: string): string {
+        if (!url)
+            return url;
+        if (url.indexOf("data:") > -1 || url.indexOf("blob:") > -1)
+            return url;
+        return url + (url.indexOf("?") > -1 ? "&" : "?") + "no-cache=" + new Date().getTime();
+    }
+
+    public static selectFiles(
+        params: {
+            isMultiple: boolean;
+            acceptExtensions: string[];
+            onFinish: (files: File[]) => void;
+        }
+    ) {
+        let id = "Flmngr-select-files-" + new Date().getTime();
+        let elForm = document.getElementById(id);
+        if (elForm != null)
+            elForm.parentNode.removeChild(elForm);
+
+        elForm = document.createElement("form");
+        elForm.setAttribute("id", id);
+        elForm.setAttribute("enctype", "multipart/form-data");
+        elForm.setAttribute("style", "display:none !important");
+        document.querySelector("body").appendChild(elForm);
+
+        let elInput: HTMLInputElement = document.createElement("input") as HTMLInputElement;
+        elInput.setAttribute("type", "file");
+        elInput.setAttribute("name", "file");
+        elForm.appendChild(elInput);
+        if (params.isMultiple)
+            elInput.setAttribute("multiple", "multiple");
+
+        if (params.acceptExtensions != null && params.acceptExtensions.length > 0) {
+            let acceptMimeTypes: string[] = [];
+            let mime = require("mime");
+            for (const ext of params.acceptExtensions)
+                acceptMimeTypes.push(mime.getType(ext));
+            elInput.setAttribute("accept", acceptMimeTypes.join("|"));
+        }
+
+        let onChange = ((elInput: HTMLInputElement, onSelected: (files: File[]) => void) => {
+            return () => {
+                let files: File[] = [];
+                for (let i = 0; i < elInput.files.length; i++)
+                    files.push(elInput.files[i]);
+                onSelected(files);
+            };
+        })(elInput, params.onFinish);
+
+        elInput.addEventListener("change", onChange);
+        elInput.click();
+    }
+
+    public static selectUrls(params: FlmngrSelectUrlsParams) {
+        let forcedParams = {
+            ...params,
+            waitForImgPenToo: false
+        };
+        Flmngr.waitForLoaded(forcedParams, () => {
+            let mergedParams = {
+                ...this.commonParams,
+                ...params
+            };
+            (window as any).flmngr.selectUrls(mergedParams);
+        });
+    }
+
+
+}
